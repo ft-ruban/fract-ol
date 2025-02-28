d749744eab398af461404257e7d4744b6e843d88
Lea Devoudelle-Royer <ldevoude@z2r10p3.42lyon.fr>
2025-02-28 16:20:44 +0100

    {feat} finished the error function and the Help parameter

--- error.c
+++ error.c
@@ -14,8 +14,31 @@ void *free_null(void *target)
 //TODO : ft_printfd to send error to the error fd related + different error messages depending of the returned code!
 int error_msg(int error_code)
 {
-    ft_printfd("Error code : %d", error_code); //put fd 2
-    return (1);
+    if (error_code == 1)
+        ft_printfd("Error code : %d\n I cannot detect any arguments fractol require at least a type of fractal\n", error_code);
+    else if (error_code == 2)
+        ft_printfd("Error code : %d\n Mandelbrot should not receive any parameters.\n", error_code);
+    else if (error_code == 3)
+        ft_printfd("Error code : %d\n Julia set require two additionnal argument that would be used to create the fractal.\n", error_code);
+    else if (error_code == 4)
+    {
+        ft_printf("To use that program you need to write as first argument the name of the fractal you are looking for\n\n");
+        ft_printf("available sets:\n-Mandelbrot\n-Julia\n\n Mandelbrot doesnt require any parameters but Julia does ");
+        ft_printf("bellow is a little list of nice looking Julia parameters.\n");
+        ft_printf("./fractol Julia 0.12 -0.77\n./fractol Julia 0.83593984038125 0.23254012009216\n");
+        ft_printf("./fractol Julia 0.79 0.15\n./fractol Julia -0.7 0.27015\n");
+        ft_printf("./fractol Julia -0.162 1.04\n./fractol Julia 0.3 -0.0\n");
+        ft_printf("./fractol Julia 0.28 0.008\n./fractol Julia 0.12 -0.77\n");
+        ft_printf("./fractol Julia -0.12 -0.77\n./fractol Julia -1.476 0.0\n");
+        ft_printf("use ESC or close the window by clicking on the window cross ");
+        ft_printf("to close the program\nto zoom in and out use the mouse wheel\n");
+        return (0);
+    }
+    else if (error_code == 5)
+        ft_printfd("Error code : %d\n I cannot recognize your arguments, check for any typos and if your fractal does exist inside of the program.\n", error_code);
+    
+    ft_printfd("do not hesitate to use the help argument (./fractol Help) to get more information!");
+    return (error_code);
 }
 
 //will determine which fractal was selected set set_num with the right numbercode verify if the number of arguments are valids and setup what need to be
@@ -44,10 +67,8 @@ int is_sets_available(char **argv, int *set_num, int argc, t_complex *c)
         return (0);
     }
     else if (!ft_strcmp (argv[1], "Help"))
-    {
-        ft_printf("topmoumoute");
         return (4);
-    }
+
     else
         return (5);
 }
