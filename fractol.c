757a558c2dd91617bfdc98ba6c3f5138a58def41
Lea Devoudelle-Royer <ldevoude@z2r11p1.42lyon.fr>
2025-03-27 13:24:43 +0100

    {feat} zoom + color valgrind clean need to norme the code and check for potential issues

--- fractol.c
+++ fractol.c
@@ -10,3 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fractol.h"
+
+void init_param(t_set_call *param) {
+    param->c = ft_calloc(1, sizeof(t_complex));
+    param->z = ft_calloc(1, sizeof(t_complex));
+    param->c->real_x = 0.0;
+    param->c->imaginary_y = 0.0;
+    param->z->real_x = 0.0;
+    param->z->imaginary_y = 0.0;
+
+    param->zoom = 4.0;
+    change_color(&param->color_factor);
+}
--- fractol.h
+++ fractol.h
@@ -6,31 +6,36 @@
 /*   By: ldevoude <ldevoude@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:31:30 by ldevoude          #+#    #+#             */
-/*   Updated: 2025/02/09 17:33:14 by ldevoude         ###   ########lyon.fr   */
+/*   Updated: 2025/02/28 16:32:31 by ldevoude         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
-#include <mlx.h>
-#include <stdlib.h>
+#include "mlx_linux/mlx.h"
 #include <stdio.h> //dlt before push
+#include <stdlib.h>
+#include "libftx/libft.h"
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
-# define C(x, y) ((x) + (y))
 # define true 1
 # define false 0
-# define WIN_HEIGH 1920
-# define WIN_WITH 1080
-
+# define MANDELBROT_ID 1
+# define JULIA_ID 2
+# define WIN_WIDTH 1920 //500 //1920
+# define WIN_HEIGHT  1080//500 //1080
+# define X_MIN -2.0
+# define X_MAX 1.0
+# define Y_MIN -1.5
+# define Y_MAX 1.5
+# define WIN_HEIGHT_HALF WIN_HEIGHT / 2.0
+# define WIN_WIDTH_HALF WIN_WIDTH / 2.0
 typedef struct s_complex
 {
-	double real_x;
-	double imaginary_y;
-	int 	ratio_y;
-	int 	ratio_x;
-}	t_complex;
+	double	real_x;
+	double	imaginary_y;
+}			t_complex;
 
 typedef struct s_img
 {
@@ -48,13 +53,42 @@ typedef struct s_mlx
 	t_img	img;
 }			t_mlx;
 
-typedef struct s_ratio
+typedef struct s_utils
 {
-	int 	ratio_y;
-	int 	ratio_x;
-}			t_ratio;
+	double 	x;
+	double 	y; //int if broke
+	int 	iter;
+	double	xtemp;
+}			t_utils;
+
+typedef struct s_set_call
+{
+	t_mlx		*screen;
+	t_complex	*c;
+	t_complex	*z;
+	double 		zoom;
+	int			mandelbrot;
+	int 		color_factor;
+}			t_set_call;
 #endif
 
-void		init_screen_mlx(t_mlx *t_mlx);
-void		mandelbrot_set(t_mlx *mlx, t_complex c);
+int 		main(int argc, char *argv[]);
+void 		init_param(t_set_call *param);
+int			close_window(t_set_call *param);
+int 		change_color(int *color_factor);
+int 		key_move(int keycode, t_set_call *param);
+int 		handle_keys(int keycode, t_set_call *param);
+int 		zoom (int type, int x, int y, t_set_call *param);
+void 		julia_formula(t_complex *z, t_complex *c, t_utils *misc_utils);
+int 		color_seed();
+int			get_color(int iter, int color_factor);
+void		*init_screen_mlx(t_mlx *t_mlx, char **argv);
+void		mandelbrot_set(t_mlx *mlx, t_complex *c, t_utils *misc_utils, t_complex *z);
+void		julia_set(t_mlx *mlx, t_complex *c, t_utils *misc_utils, t_complex *z);
 void		my_mlx_pixel_put(t_img *data, int x, int y, int color);
+int 		is_sets_available(char **argv, int argc, t_set_call *param);
+int 		error_msg(int error_code);
+void 		*free_null(void  *target);
+void 		pixel_coordinates(t_complex *center, t_utils *pixel, t_complex *out, double zoom);
+void 		initialize_complex(t_set_call *param, t_complex *z, t_complex *c, t_utils *misc_utils);
+int 		set(t_set_call *param);
